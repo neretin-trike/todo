11d9b9074c85380f36dc8d7f52d69cbb311d132a
neretin-trike <hawktrike@gmail.com>
2019-03-27 01:18:19 +0700

    update*js): добавление методов для взаимодействия с задачами

--- src/api/apiManager.js
+++ src/api/apiManager.js
@@ -2,8 +2,11 @@ import { BASE_URL } from '../constants/config.js';
 
 let api = null;
 
-function getInitializedApi(url, myMethod = "GET", myHeaders = {}, data = {}) {
+function getInitializedApi(url, myMethod = "GET", data = {}) {
   if (api) return api; // return initialized api if already initialized.
+
+  let myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${store.state.user.token}`);
 
   let initOptions = {
       mode:'cors',
@@ -12,14 +15,46 @@ function getInitializedApi(url, myMethod = "GET", myHeaders = {}, data = {}) {
       header: myHeaders,
       body: data,
   }
+
+  if (initOptions.method === "POST") {
+      myHeaders.append("Content-Type",'multipart/form-data')
+  }
+
   return ( api = fetch(BASE_URL+url, initOptions) );
 }
 
-function getMethod(url, headers) {
-    return getInitializedApi(url, "GET", headers)
+function getMethod(url) {
+    return getInitializedApi(url, "GET")
 }
 
-function postMethod(url, headers, data) {
-    return getInitializedApi(url, "POST", headers, data)
+function postMethod(url, data) {
+    return getInitializedApi(url, "POST",data)
 }
 
+function deleteMethod(url) {
+    return getInitializedApi(url, "DELETE")
+}
+
+
+
+
+
+function getTaskList(page = 1) {
+    return getMethod(`/api/task/list/${page}`);
+}
+
+function getTask(id = 1) {
+    return getMethod(`/api/task/${id}`);
+}
+
+function saveTask(formData) {
+    return postMethod(`/api/task/save`, formData);
+}
+
+function editTask(id, formData) {
+    return postMethod(`/api/task/save/${id}`, formData);
+}
+
+function deleteTask(id) {
+ return deleteMethod(`/api/task/${id}`); 
+}
